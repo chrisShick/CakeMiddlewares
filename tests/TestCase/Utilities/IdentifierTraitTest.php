ad30ffe11e94123bd2f47d66bab7a79752e3e96b
Christopher HIckingbottom <christopherhickingbottom@Christophers-MacBook-Pro.local>
2017-03-10 17:04:14 -0600

    adding IdentifierTraitTest

--- tests/TestCase/Utilities/IdentifierTraitTest.php
+++ tests/TestCase/Utilities/IdentifierTraitTest.php
@@ -1,10 +1,29 @@
 <?php
 namespace chrisShick\CakeMiddlewares\Test\TestCase\Utilities;
 
+use Cake\Http\ServerRequest;
 use Cake\TestSuite\TestCase;
+use chrisShick\CakeMiddlewares\Utilities\IdentifierTrait;
+use FriendsOfCake\TestUtilities\AccessibilityHelperTrait;
+use Psr\Http\Message\ServerRequestInterface;
+
+class testClass {
+    use IdentifierTrait;
+
+    protected $_defaultConfig = [];
+}
 
 class UtilitesTestCase extends TestCase
 {
+    use AccessibilityHelperTrait;
+
+    /**
+     * Test object
+     *
+     * @var object
+     */
+    private $testObject;
+
     /**
      * setUp method
      *
@@ -13,13 +32,64 @@ class UtilitesTestCase extends TestCase
     public function setUp()
     {
         parent::setUp();
+
+        $this->testObject = new testClass();
+        $this->setReflectionClassInstance($this->testObject);
     }
 
-    public function testSetIdentifier()
-    {
-    }
-
+    /**
+     * Test setIdentifierConfig
+     */
     public function testSetIdentifierConfig()
     {
+        $config = [
+            'test' => 'value'
+        ];
+
+        $expected = $config + [
+            'identifier' => function (ServerRequestInterface $request) {
+                return $request->clientIp();
+            }
+        ];
+
+        $actual = $this->callProtectedMethod('setIdentifierConfig', $config, $this->testObject);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test setIdentifier
+     */
+    public function testSetIdentifier()
+    {
+        $request = new ServerRequest([
+            'environment' => [
+                'REMOTE_ADDR' => '192.168.211.12'
+            ]
+        ]);
+
+        $expected = '192.168.211.12';
+        $this->callProtectedMethod('setIdentifier', $request, $this->testObject);
+        $actual = $this->getProtectedProperty('identifier', $this->testObject);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * Test setIdentifierFailed
+     */
+    public function testSetIdentifierFailed()
+    {
+        $this->testObject = new testClass();
+        $this->setReflectionClassInstance($this->testObject);
+
+        $request = new ServerRequest([
+            'environment' => [
+                'REMOTE_ADDR' => '192.168.211.12'
+            ]
+        ]);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->callProtectedMethod('setIdentifier', $request, $this->testObject);
     }
 }
